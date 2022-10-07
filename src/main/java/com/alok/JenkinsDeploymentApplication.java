7184eaf7ca3837125a81bce559432d34d33d6ef4
AlokSachan <alok.sachan.83@gmail.com>
2022-10-07 14:54:22 +0530

    modify main method class

--- src/main/java/com/alok/JenkinsDeploymentApplication.java
+++ src/main/java/com/alok/JenkinsDeploymentApplication.java
@@ -2,12 +2,22 @@ package com.alok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class JenkinsDeploymentApplication {
+public class JenkinsDeploymentApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsDeploymentApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDeploymentApplication.class, args);
 	}
 
 }
+
